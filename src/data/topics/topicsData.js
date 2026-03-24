c7a7c38eb14d3549aca93959f7ddc3ee369a6bfa
Tiia Pitkänen <tiia1.pitkanen@gmail.com>
2026-03-24 19:17:55 +0200

    deleted topics

--- src/data/topics/topicsData.js
+++
@@ -1,9 +0,0 @@
-import { managementTopics } from "./management";
-import { blockchainTopics } from "./blockchain";
-import { aiTopics } from "./ai";
-import { dataTopics } from "./data";
-
-
-// Keep the main catalog lean: non-data topics + one aggregated data section
-const topics = [managementTopics, blockchainTopics, aiTopics, dataTopics];
-export default topics;
